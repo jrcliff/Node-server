673011a8b72452c991353cfcb2eeae217b29ade1
jrcliff <justoon7.jc@gmail.com>
2021-06-03 21:15:00 -0400

    practicing restful routes for app

--- server/index.js
+++ server/index.js
@@ -8,6 +8,10 @@ app.get("/api", (req, res) => {
     res.json({message: 'Hello from your server'})
 });
 
+app.get("/hello", (req, res) => {
+    res.json({message: 'Hello World!!!!'})
+})
+
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
